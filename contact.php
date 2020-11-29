19eea50c4f85b4566084eb7e22c086b8ab2aa454
Himanshu <himanshupatkar27@gmail.com>
2020-11-29 14:10:33 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'Demo contact form <contact@himanshupatkar.in>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <contact@himanshupatkar.in>';
+$sendTo = <contact@himanshupatkar.in>';
 
 // subject of the email
 $subject = 'New message from contact form';
